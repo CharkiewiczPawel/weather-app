40a0b637f03acf659d3d9bdfa4b954639f01ca61
CharkiewiczPawel <charkiewicz.pawe4@gmail.com>
2022-06-27 10:11:23 +0200

    improved time display

--- src/components/CurrentDate.tsx
+++ src/components/CurrentDate.tsx
@@ -2,26 +2,29 @@ import sun from "../images/weather/sun.png"
 import moon from "../images/weather/moon.png"
 
 
-const CurrentDate: React.FC=()=>{
-    
-    const data:Date=new Date();
-    const todaysDate:String=`Data:${data.getDate()}.${data.getMonth()+1}.${data.getFullYear()}`;
-    const currentHours:String=`Godzina:${data.getHours()}:${data.getMinutes()}`;
+const CurrentDate: React.FC = () => {
 
-    const compareData:number=data.getHours();
-    let weatherPicture=(sun)
-    if(compareData>4&&compareData<22){
-        weatherPicture=(sun)
-    }else weatherPicture=(moon)
+  const data: Date = new Date();
+  const todaysDate: String = `Data:${data.getDate()}.${data.getMonth() + 1}.${data.getFullYear()}`;
+  let currentHours: String
+  if (data.getMinutes() < 10) { currentHours = `Godzina:${data.getHours()}:0${data.getMinutes()}` }
+  else { currentHours = `Godzina:${data.getHours()}:${data.getMinutes()}` }
 
-    
-    
-    return(
+
+  const compareData: number = data.getHours();
+  let weatherPicture = (sun)
+  if (compareData > 4 && compareData < 22) {
+    weatherPicture = (sun)
+  } else weatherPicture = (moon)
+
+
+
+  return (
     <div className="withDateBoard">
       <img className="displayWeatherPicture" src={weatherPicture} />
       <p>{todaysDate}</p>
       <p>{currentHours}</p>
-    </div>        
-    )
-    }
-    export default CurrentDate
+    </div>
+  )
+}
+export default CurrentDate
